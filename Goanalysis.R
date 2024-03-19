473b11022568c1f7b68749a81b008d38655d7965
Aakankshas90 <151615266+Aakankshas90@users.noreply.github.com>
2024-03-19 11:27:25 +0800

    Update Goanalysis.R

--- Goanalysis.R
+++ Goanalysis.R
@@ -1,23 +1,14 @@
-BiocManager::install("GOstats")
+BiocManager::install("AnnotationDbi")
 BiocManager::install("org.At.tair.db")
-BiocManager::install("pathview")
+BiocManager::install("clusterProfiler")
 BiocManager::install("enrichplot")
 library("AnnotationDbi")
-library("GOstats")
 library("clusterProfiler")
-library("GenomicFeatures")
-library("RColorBrewer")
-library("Rgraphviz")
 library("org.At.tair.db")
 library("dplyr")
 library("pathview")
-help('clusterProfiler')
-help('goplot')
 
 
-keytypes(txdb)
-keytypes(org.At.tair.db)
-setwd('/data/2023_DAPseq/HDL_DAP-seq/11chipseeker/bammerge/')
 ##############################################################################################################################################
 HDLmerge_entrez <- HDL_annot$geneId
 
@@ -272,123 +263,4 @@ dotplot(mhdmerge_all,  x = "Count",
         color = "qvalue",
         showCategory = 20,
         font.size = 10, split="ONTOLOGY")
-##############################################################################################################################################
-getwd()
-setwd('/data/2023_DAPseq/HDL_DAP-seq/11chipseeker/bammerge')
-list.files()
-keytypes("org.At.tair.db")
-###########################################################################################################################
-# Get GOstats results for GO categories and KEGG pathways
-calcGOstats <- function(universeIDs, targetIDs, outGO, outKEGG)
-{
-  # universeIDs : universe ids
-  # targetIDs : target ids
-  # outFile : file with significant GO categories
-  # outKEGG : significant KEGG pathways
-  # Specify parameters for hypergeometric test against GO categories biological process
-  paramsBP <- new("mhd_wt_bamcommon_annot",
-                  geneIds=targetIDs,
-                  universeGeneIds=universeIDs,
-                  annotation="org.At.tair.db",
-                  ontology="ALL",
-                  pvalueCutoff=0.05,
-                  conditional=FALSE,
-                  testDirection="over")
-  
-  # Run enrichment against Go categories - Biological process
-  hgOver <- hyperGTest(paramsBP)
-  GOtab <- summary(hgOver)
-  write.table(GOtab, file=outGO, sep="\t", col.names=T, row.names=F, quote=F)
-  
-  paramsKEGG <- new("KEGGHyperGParams",
-                    geneIds=targetIDs,
-                    universeGeneIds=universeIDs,
-                    annotation="org.At.tair.db",
-                    pvalueCutoff=0.1,
-                    categoryName="KEGG",
-                    testDirection="over")
-  
-  # Run enrichment against Go categories - Biological process
-  hgOverKEGG <- hyperGTest(paramsKEGG)
-  KEGGtab <- summary(hgOverKEGG)
-  
-  # Retrieve gene ids found in enriched pathways
-  keggMaps <- lapply(KEGGtab$KEGGID, function(x) unlist(mget(x, org.At.tairPATH2TAIR)))
-  
-  # subset the selected genes based on those in the mappings
-  keggSelected <- lapply(keggMaps, function(x) targetIDs[targetIDs %in% x])
-  
-  # join together with a semicolon to make up the last column
-  KEGGtab$inGenes <- unlist(lapply(keggSelected, function(x) paste(x, collapse=";")))
-  write.table(KEGGtab, file=outKEGG, sep="\t", col.names=T, row.names=F, quote=F)
-}
 
-############################################################################################################################
-# Calculate for Crysp response
-# Read the results file
-res <- read.table("Crysp_1h_vs_PBS_1h_annot.txt", sep="\t", header=T, fill = T, quote = "")
-universeIDs <- as.character(res$gene_id)
-# Up-regulated in Crysp
-up <- res[res$prob > 0.95 & res$M > 0,]
-upIDs <- as.character(unique(up$gene_id))
-upIDs <- upIDs[!is.na(upIDs)]
-calcGOstats(universeIDs = universeIDs, targetIDs = upIDs, outGO = "UP_in_Crysp_GO.txt", outKEGG = "UP_in_Crysp_KEGG.txt" ) 
-
-# Down-regulated in Crysp
-down <- res[res$prob > 0.95 & res$M < 0,]
-downIDs <- as.character(unique(down$gene_id))
-downIDs <- downIDs[!is.na(downIDs)]
-calcGOstats(universeIDs = universeIDs, targetIDs = downIDs, outGO = "DOWN_in_Crysp_GO.txt", outKEGG = "DOWN_in_Crysp_KEGG.txt" ) 
-
-# all significant in Crysp
-all <- res[res$prob > 0.95,]
-allIDs <- as.character(unique(all$gene_id))
-allIDs <- allIDs[!is.na(allIDs)]
-calcGOstats(universeIDs = universeIDs, targetIDs = allIDs, outGO = "AllSig_in_Crysp_GO.txt", outKEGG = "AllSig_in_Crysp_KEGG.txt" ) 
-
-###############################################################################################################################
-# Calculate for Gyr response
-# Read the results file
-res <- read.table("Gyr_1h_vs_PBS_1h_annot.txt", sep="\t", header=T, fill = T, quote = "")
-universeIDs <- as.character(res$gene_id)
-# Up-regulated in Gyr
-up <- res[res$prob > 0.95 & res$M > 0,]
-upIDs <- as.character(unique(up$gene_id))
-upIDs <- upIDs[!is.na(upIDs)]
-calcGOstats(universeIDs = universeIDs, targetIDs = upIDs, outGO = "UP_in_Gyr_GO.txt", outKEGG = "UP_in_Gyr_KEGG.txt" ) 
-
-# Down-regulated in Gyr
-down <- res[res$prob > 0.95 & res$M < 0,]
-downIDs <- as.character(unique(down$gene_id))
-downIDs <- downIDs[!is.na(downIDs)]
-calcGOstats(universeIDs = universeIDs, targetIDs = downIDs, outGO = "DOWN_in_Gyr_GO.txt", outKEGG = "DOWN_in_Gyr_KEGG.txt" ) 
-
-# all significant in Gyr
-all <- res[res$prob > 0.95,]
-allIDs <- as.character(unique(all$gene_id))
-allIDs <- allIDs[!is.na(allIDs)]
-calcGOstats(universeIDs = universeIDs, targetIDs = allIDs, outGO = "AllSig_in_Gyr_GO.txt", outKEGG = "AllSig_in_Gyr_KEGG.txt" ) 
-
-###########################################################################################################################
-# draw pathway
-pathway_ids <- c("04626", "00592", "04075", "04144", "04141", "00040", "00270", "04070")
-
-# read expression data from Crysp response
-crysp_exp <- read.table("expression_data/expressions_vsd.txt", header = T, sep = "\t")
-crysp_exp_diff <- crysp_exp[,c(3,4)] - crysp_exp[,c(1,2)]
-crysp_exp_diff <- as.matrix(crysp_exp_diff)
-
-# read expression data from Gyr response
-gyr_exp <- read.table("../../expression_data/expressions_vsd.txt", header = T, sep = "\t")
-gyr_exp_diff <- gyr_exp[,c(5,6)] - gyr_exp[,c(1,2)]
-gyr_exp_diff <- as.matrix(gyr_exp_diff)
-
-setwd("../Gyr_pathway_images/")
-sapply(pathway_ids, function(pid) pathview(gene.data = gyr_exp_diff, 
-                                           pathway.id = pid, 
-                                           species = "ath",
-                                           kegg.native=T, 
-                                           sign.pos="bottomleft",
-                                           gene.annotpkg="org.At.tair.db",
-                                           out.suffix = pid,
-                                           gene.idtype = "KEGG"))
